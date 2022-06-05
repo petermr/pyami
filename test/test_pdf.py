e74a6bb034c38ebf2d366096ba30cad7c3b91ed1
petermr <peter.murray.rust@googlemail.com>
2022-06-05 09:12:51 +0100

    added PDFMiner tests for textboxes and fonts.

--- test/test_pdf.py
+++ test/test_pdf.py
@@ -367,6 +367,62 @@ class PDFTest(unittest.TestCase):
 
     # https://towardsdatascience.com/pdf-text-extraction-in-python-5b6ab9e92dd
 
+    def test_pdfminer_layout(self):
+        from pdfminer.layout import LTTextLineHorizontal, LTTextBoxHorizontal
+        # need to pass in laparams, otherwise pdfplumber page would not
+        # have high level pdfminer layout objects, only LTChars.
+        pdf = pdfplumber.open(PMC1421, laparams={})
+        page = pdf.pages[0].layout
+        for element in page:
+            if isinstance(element, LTTextLineHorizontal):
+                print(f"text: ({element.bbox}) .. {element.get_text()}")
+            if isinstance(element, LTTextBoxHorizontal):
+                for line in element:
+                    print(f"....textbox: ({line.bbox}) {line.get_text()}")
+
+# https://stackoverflow.com/questions/34606382/pdfminer-extract-text-with-its-font-information
+
+    def test_pdfminer_font(self):
+        from pathlib import Path
+        from typing import Iterable, Any
+
+        from pdfminer.high_level import extract_pages
+
+        def show_ltitem_hierarchy(o: Any, depth=0):
+            """Show location and text of LTItem and all its descendants"""
+            if depth == 0:
+                print('element                        fontname             text')
+                print('------------------------------ -------------------- -----')
+
+            print(
+                f'{get_indented_name(o, depth):<30.30s} '
+                f'{get_optional_fontinfo(o):<20.20s} '
+                f'{get_optional_text(o)}'
+            )
+
+            if isinstance(o, Iterable):
+                for i in o:
+                    show_ltitem_hierarchy(i, depth=depth + 1)
+
+        def get_indented_name(o: Any, depth: int) -> str:
+            """Indented name of class"""
+            return '  ' * depth + o.__class__.__name__
+
+        def get_optional_fontinfo(o: Any) -> str:
+            """Font info of LTChar if available, otherwise empty string"""
+            if hasattr(o, 'fontname') and hasattr(o, 'size'):
+                return f'{o.fontname} {round(o.size)}pt'
+            return ''
+
+        def get_optional_text(o: Any) -> str:
+            """Text of LTItem if available, otherwise empty string"""
+            if hasattr(o, 'get_text'):
+                return o.get_text().strip()
+            return ''
+
+        path = Path(PMC1421)
+        pages = extract_pages(path)
+        show_ltitem_hierarchy(pages)
 
 # ==============================
 
