6e1e36e961ae0f4843e24658157895e82e5eb54a
petermr <peter.murray.rust@googlemail.com>
2022-07-22 17:23:22 +0100

    added ami_chem

---
+++ py4ami/ami_chem.py
@@ -0,0 +1,17 @@
+
+
+class AmiChem:
+
+    def __init__(self):
+        pass
+
+
+class AmiSpect:
+
+    def __init__(self):
+        pass
+
+    def read_condensed_data(self, html):
+        pass
+
+    
